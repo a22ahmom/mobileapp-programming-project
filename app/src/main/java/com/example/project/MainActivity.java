389d87795ec5244e1a99da7d8c1703bb289180a9
Sahand Omarzai <a22ahmom@student.his.se>
2023-05-22 15:30:54 +0200

    implemented code to handle onClickListener in the main page

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.project;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -19,10 +22,12 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener{
+
     private final String JSON_URL = "https://mobprog.webug.se/json-api?login=a22ahmom";
     private final String JSON_FILE = "wildanimals.json";
     private ArrayList<WildAnimals> wildAnimalsArrayList;
     RecyclerView recyclerView;
+
     private String readFile(String fileName) {
         try {
             //noinspection CharsetObjectCanBeUsed
@@ -41,18 +46,25 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         setSupportActionBar(toolbar);
         wildAnimalsArrayList = new ArrayList<>();
         recyclerView = findViewById(R.id.recycler_view);
+        Button aboutScreenButton = findViewById(R.id.aboutScreen);
+        aboutScreenButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, AboutScreen.class);
+                startActivity(intent);
+            }
+        });
 
         new JsonTask(this).execute("https://mobprog.webug.se/json-api?login=a22ahmom");
 
         String s = readFile("wildanimals.json");
         Log.d("MainActivity","The following text was found in textfile:\n\n"+s);
-
     }
 
     @SuppressLint("NotifyDataSetChanged")
     @Override
     public void onPostExecute(String json) {
-        Log.d("Mainactivity ==>", json);
+        //Log.d("Mainactivity ==>", json);
 
         Gson gson = new Gson();
         Type type = new TypeToken<ArrayList<WildAnimals>>(){}.getType();
@@ -67,6 +79,7 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         for(int i = 0; i < wildAnimalsArrayList.size(); i++) {
             Log.d("Mainactivity ==>", "Hittade ett vilt djur " + wildAnimalsArrayList.get(i).getName());
         }
+
     }
 }
 
