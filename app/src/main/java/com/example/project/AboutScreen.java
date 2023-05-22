c90c89d85fa3bc30af4f6ef29677bbb591871eda
Sahand Omarzai <a22ahmom@student.his.se>
2023-05-22 16:24:50 +0200

    created an about html file, implemented showInternalWebPage and removed AppBarLayout

--- app/src/main/java/com/example/project/AboutScreen.java
+++ app/src/main/java/com/example/project/AboutScreen.java
@@ -2,18 +2,25 @@ package com.example.project;
 
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutScreen extends AppCompatActivity {
+    private WebView webView;
+
+    public void showInternalWebPage(){
+        webView.loadUrl("file:///android_asset/about.html");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
         TextView textView = findViewById(R.id.textview);
+        webView = findViewById(R.id.webview);
 
         Bundle extras = getIntent().getExtras();
         if(extras != null){
