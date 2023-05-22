03b1bbd2c5b9c20674ad5d308f147444d254518f
Sahand Omarzai <a22ahmom@student.his.se>
2023-05-22 09:38:29 +0200

    added a method to read internal file which in this case is wildanimals.json

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,27 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
 public class MainActivity extends AppCompatActivity {
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=a22ahmom";
+
+    private String readFile(String fileName) {
+        try {
+            //noinspection CharsetObjectCanBeUsed
+            return new Scanner(getApplicationContext().getAssets().open(fileName), Charset.forName("UTF-8").name()).useDelimiter("\\A").next();
+        } catch (IOException e) {
+            Log.d("TAG", "Could not read file: " + fileName);
+            return null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        String s = readFile("wildanimals.json");
+        Log.d("MainActivity","The following text was found in textfile:\n\n"+s);
+
     }
 
 }
