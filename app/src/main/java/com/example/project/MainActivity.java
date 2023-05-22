2c7ba479a5072a6b779eb8fbf001fcf5a20757f8
Sahand Omarzai <a22ahmom@student.his.se>
2023-05-22 11:44:19 +0200

    implemented JsonTask object to execute the URl

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -18,7 +18,7 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener{
     private final String JSON_URL = "https://mobprog.webug.se/json-api?login=a22ahmom";
     private ArrayList<WildAnimals> wildAnimalsArrayList;
     RecyclerView recyclerView;
@@ -38,8 +38,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        wildAnimalsArrayList = new ArrayList<>();
         recyclerView = findViewById(R.id.recycler_view);
 
+        new JsonTask(this).execute("https://mobprog.webug.se/json-api?login=a22ahmom");
 
         String s = readFile("wildanimals.json");
         Log.d("MainActivity","The following text was found in textfile:\n\n"+s);
