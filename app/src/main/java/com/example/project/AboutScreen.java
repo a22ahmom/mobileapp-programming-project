d2b43c947a3e693becbc2ff18ad5d20cd8d8e285
Sahand Omarzai <a22ahmom@student.his.se>
2023-05-22 15:15:11 +0200

    implemented code to handle bundle and a clicklistener in AboutScreen file

--- app/src/main/java/com/example/project/AboutScreen.java
+++ app/src/main/java/com/example/project/AboutScreen.java
@@ -1,8 +1,32 @@
 package com.example.project;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutScreen extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+        TextView textView = findViewById(R.id.textview);
 
+        Bundle extras = getIntent().getExtras();
+        if(extras != null){
+            String info = extras.getString("info");
+            textView.setText(info);
+        }
+
+        Button returnToMainPageButton = findViewById(R.id.returnToMainPage);
+        returnToMainPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                finish();
+            }
+        });
+    }
 }
