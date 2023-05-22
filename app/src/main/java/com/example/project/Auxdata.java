06b18d13d804117aef66a5b6a07a988f81fbd7d2
Sahand Omarzai <a22ahmom@student.his.se>
2023-05-22 09:59:26 +0200

    created a WildAnimals class and Auxdata class

---
+++ app/src/main/java/com/example/project/Auxdata.java
@@ -0,0 +1,11 @@
+package com.example.project;
+
+public class Auxdata {
+
+    private String wiki;
+    private String img;
+
+    public String getWiki() {
+        return wiki;
+    }
+}
---
+++ app/src/main/java/com/example/project/WildAnimals.java
@@ -0,0 +1,68 @@
+package com.example.project;
+
+import androidx.annotation.NonNull;
+
+import com.google.gson.annotations.SerializedName;
+
+public class WildAnimals {
+    private String ID;
+    private String name;
+    private String type;
+    private String company;
+    private String location;
+    private String category;
+    @SerializedName("size")
+    private int meter;
+    @SerializedName("cost")
+    private int feet;
+    private Auxdata auxdata;
+
+    public WildAnimals(String name){
+        this.name = name;
+    }
+
+    public WildAnimals(String ID, String name, String type, String company, String location, String category, int meter, int feet){
+        this.ID = ID;
+        this.name = name;
+        this. type = type;
+        this.company = company;
+        this.location = location;
+        this.category = category;
+        this.meter = meter;
+        this.feet = feet;
+    }
+
+
+    public void setID(String id) {
+        this.ID = id;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Auxdata getAuxdata() {
+        return auxdata;
+    }
+
+    @NonNull
+    @Override
+    public String toString() { return name; }
+
+}
