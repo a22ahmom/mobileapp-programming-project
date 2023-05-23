2f72861f46877ab3b7f61f00d629ac03d431f466
Sahand Omarzai <a22ahmom@student.his.se>
2023-05-23 19:02:17 +0200

    added the ThirdActivity in the manifest file and implemented onCreate method

--- app/src/main/java/com/example/project/ThirdActivity.java
+++ app/src/main/java/com/example/project/ThirdActivity.java
@@ -1,4 +1,15 @@
 package com.example.project;
 
-public class ThirdActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.third_activity);
+    }
 }
